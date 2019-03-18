44c902fc0b888673af281b95a7afaaf525d42c02
Wecode <mashleyalpha@gmail.com>
2019-03-18 15:29:56 +0200

    location model modified

--- gllry/models.py
+++ gllry/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Location(models.Model):
+class Locations(models.Model):
     photo_location = models.CharField(max_length=50)
 
     def save_location(self):
